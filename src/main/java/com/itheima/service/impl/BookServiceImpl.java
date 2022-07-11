5ddead6c16b2fdf97c9249ac4d252f8e2234b3dd
lhz <liuhuzhidiyi@163.com>
2022-07-11 15:14:06 +0800

    项目初始结构

--- src/main/java/com/itheima/service/impl/BookServiceImpl.java
+++ src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -5,11 +5,17 @@ import com.itheima.dao.impl.BookDaoImpl;
 import com.itheima.service.BookService;
 
 public class BookServiceImpl implements BookService {
-    private BookDao bookDao=new BookDaoImpl();
+
+    //5.删除业务层中使用new的方式创建的dao对象
+    private BookDao bookDao;
 
     public void save() {
         System.out.println("book service save ...");
         bookDao.save();
     }
 
+    //6.提供对应set方法
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
 }
